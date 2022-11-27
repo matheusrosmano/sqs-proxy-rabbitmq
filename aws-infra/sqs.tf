5cf676e487e103d16b377561aa49222983956891
Matheus Rosmaninho <matheusrosmano@gmail.com>
2022-11-27 10:47:08 -0300

    fix: Ajust the name

--- aws-infra/sqs.tf
+++ aws-infra/sqs.tf
@@ -11,7 +11,7 @@ resource "aws_sqs_queue" "sqs_proxy_rabbitmq_queue" {
         "Effect": "Allow",
         "Principal": "*",
         "Action": "sqs:SendMessage",
-        "Resource": "arn:aws:sqs:*:*:${aws_sqs_queue.sqs_proxy_rabbitmq_queue.name}",
+        "Resource": "arn:aws:sqs:*:*:${var.preFixName}-queue",
         "Condition": {
           "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.sqs_proxy_rabbitmq_bucket.arn}" }
         }
