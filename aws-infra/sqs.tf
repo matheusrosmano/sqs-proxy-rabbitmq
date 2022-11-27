dfc3f2bd22eb0d5c5abb3a27432c29eb1588754b
Matheus Rosmaninho <matheusrosmano@gmail.com>
2022-11-27 10:45:01 -0300

    fix: Create policy to s3 access sqs

--- aws-infra/sqs.tf
+++ aws-infra/sqs.tf
@@ -3,6 +3,23 @@ resource "aws_sqs_queue" "sqs_proxy_rabbitmq_queue" {
   sqs_managed_sse_enabled = true
   visibility_timeout_seconds = 60
 
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "sqs:SendMessage",
+        "Resource": "arn:aws:sqs:*:*:${aws_sqs_queue.sqs_proxy_rabbitmq_queue.name}",
+        "Condition": {
+          "ArnEquals": { "aws:SourceArn": "${aws_s3_bucket.sqs_proxy_rabbitmq_bucket.arn}" }
+        }
+      }
+    ]
+  }
+  POLICY
+
   tags = {
     Name = "SQS para o sqs proxy rabbitmq"
     Type = "SQS"
