ec7de4e2bea3af7179e3d8a085e4b115f7af1de8
Matheus Rosmaninho <matheusrosmano@gmail.com>
2022-11-27 10:51:11 -0300

    fix: Wait create all resources of bucket

--- aws-infra/s3.tf
+++ aws-infra/s3.tf
@@ -49,6 +49,8 @@ resource "aws_s3_bucket_notification" "sqs_proxy_rabbitmq_bucket_notification" {
 
   depends_on = [
     aws_s3_bucket.sqs_proxy_rabbitmq_bucket,
+    aws_s3_bucket_server_side_encryption_configuration.sqs_proxy_rabbitmq_bucket_encryption,
+    aws_s3_bucket_versioning.sqs_proxy_rabbitmq_bucket_versioning,
     aws_sqs_queue.sqs_proxy_rabbitmq_queue
   ]
 }
